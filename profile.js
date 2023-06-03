cac5d2db54e8b5b9faf6d12fc47583a7cfe0f404
Sara Peel <sarampeel@gmail.com>
2023-06-03 12:24:01 -0600

    Fixed code structure and deleted unused pictures

--- profile.js
+++ profile.js
@@ -1,18 +1,35 @@
+const colorFilters = [
+    "brightness(0) saturate(100%) invert(92%) sepia(23%) saturate(7148%) hue-rotate(147deg) brightness(93%) contrast(108%)",
+    "brightness(0) saturate(100%) invert(80%) sepia(18%) saturate(1715%) hue-rotate(73deg) brightness(98%) contrast(95%)"
+];
+let currentFill = 0;
+let currentOutline = 0;
+
 class Profile {
     constructor() {
-        // do some cool stuff
-
-        // set player username at the top of the screen
-        const playerNameEl = document.querySelector('.player-name');
-        playerNameEl.textContent = this.getPlayerName();
+        // set player username at the top of the screen 
+        this.setPlayerName();
+        // do 
     }
 
-    getPlayerName() {
-        return localStorage.getItem('username') ?? 'Mystery player';
+    setPlayerName() {
+        const playerNameEl = document.querySelector('.player-name');
+        playerNameEl.textContent = localStorage.getItem('username') ?? 'Mystery player';
     }
 }
 
-const profile = new Profile();
+// const profile = new Profile();
+
+class Workshop {
+    constructor() {
+        // declare/retreive profile
+        let profile = new Profile();
+        // fill page with current profile data
+        // set listeners for all the buttons that adjust the profile when clicked
+    }
+
+    // probably some functions for the actual listener events
+}
 
 function changeName() {
     const nameEl = document.querySelector("#slug");
@@ -22,3 +39,4 @@ function changeName() {
     slugNameEl.textContent = nameEl.value;
 }
 
+let workshop = new Workshop();
